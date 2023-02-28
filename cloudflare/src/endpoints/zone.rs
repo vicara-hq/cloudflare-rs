ae3fd4b508c50835a682d154f97a0f27cab6b814
Rakshith Ravi <rakshith.ravi@gmx.com>
2023-03-01 00:05:08 +0530

    Removed unwanted things from zone

--- cloudflare/src/endpoints/zone.rs
+++ cloudflare/src/endpoints/zone.rs
@@ -1,11 +1,8 @@
-use crate::endpoints::{account::AccountDetails, plan::Plan};
 use crate::framework::{
     endpoint::{Endpoint, Method},
     response::ApiResult,
 };
 use crate::framework::{OrderDirection, SearchMatch};
-use chrono::offset::Utc;
-use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 
 /// List Zones
@@ -69,7 +66,7 @@ pub struct DeleteZone<'a> {
 }
 impl<'a> Endpoint<(), (), ()> for DeleteZone<'a> {
     fn method(&self) -> Method {
-        Method::Delete
+        Method::DELETE
     }
 
     fn path(&self) -> String {
@@ -117,44 +114,11 @@ pub enum Status {
     Deactivated,
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "lowercase", tag = "type")]
-pub enum Owner {
-    User {
-        id: Option<String>,
-        email: Option<String>,
-    },
-    Organization {
-        id: Option<String>,
-        name: Option<String>,
-    },
-}
-
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
 pub enum Type {
     Full,
     Partial,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct HostingPartner {
-    /// Host company name
-    pub name: String,
-    /// The host's website URL
-    pub website: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Meta {
-    /// Maximum custom certificates that can be uploaded/used.
-    pub custom_certificate_quota: u32,
-    /// Maximum page rules that can be created.
-    pub page_rule_quota: u32,
-    /// Indicates if URLs on the zone have been identified as hosting phishing content.
-    pub phishing_detected: bool,
-    /// Indicates whether the zone is allowed to be connected to multiple Railguns at once
-    pub multiple_railguns_allowed: bool,
 }
 
 /// A Zone is a domain name along with its subdomains and other identities
@@ -165,53 +129,8 @@ pub struct Zone {
     pub id: String,
     /// The domain name
     pub name: String,
-    /// Information about the account the zone belongs to
-    pub account: AccountDetails,
-    /// A list of beta features in which the zone is participating
-    pub betas: Option<Vec<String>>,
-    /// When the zone was created
-    pub created_on: DateTime<Utc>,
-    /// Exists only with a deactivated status and indicates the reason the zone is not resolving on
-    /// the Cloudflare network.
-    pub deactivation_reason: Option<String>,
-    /// The interval (in seconds) from when development mode expires (positive integer) or last
-    /// expired (negative integer) for the domain. If development mode has never been enabled, this
-    /// value is 0.
-    pub development_mode: i32,
-    /// Hosting partner information, if the zone signed up via a Cloudflare hosting partner
-    pub host: Option<HostingPartner>,
-    /// Metadata about the domain.
-    pub meta: Meta,
-    /// When the zone was last modified
-    pub modified_on: DateTime<Utc>,
-    /// Cloudflare-assigned name servers. This is only populated for zones that use Cloudflare DNS
-    pub name_servers: Vec<String>,
-    /// DNS host at the time of switching to Cloudflare
-    pub original_dnshost: Option<String>,
-    /// Original name servers before moving to Cloudflare
-    pub original_name_servers: Option<Vec<String>>,
-    /// Registrar for the domain at the time of switching to Cloudflare
-    pub original_registrar: Option<String>,
-    /// Information about the owner of the zone
-    pub owner: Owner,
-    /// Indicates if the zone is only using Cloudflare DNS services. A true value means the zone
-    /// will not receive security or performance benefits.
-    pub paused: bool,
-    /// Available permissions on the zone for the current user requesting the item
-    pub permissions: Vec<String>,
-    /// A zone plan
-    pub plan: Option<Plan>,
-    /// A zone plan
-    pub plan_pending: Option<Plan>,
     /// Status of the zone
     pub status: Status,
-    /// An array of domains used for custom name servers. This is only available for Business and
-    /// Enterprise plans.
-    pub vanity_name_servers: Option<Vec<String>>,
-    /// A full zone implies that DNS is hosted with Cloudflare. A partial zone is typically a
-    /// partner-hosted zone or a CNAME setup.
-    #[serde(rename = "type")]
-    pub zone_type: Type,
 }
 
 // TODO: This should probably be a derive macro
